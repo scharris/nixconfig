df3a6d7e59cbb1534fdcbd46e5833dd3b54348ad
Stephen Harris <steveofar@hotmail.com>
2022-11-29 10:47:22 -0600

    Add some zsh customization.

--- home.nix
+++ home.nix
@@ -21,22 +21,29 @@
   home.file.".editorconfig".source = ./editorconfig;
 
   programs.bash.enable = true;
-  programs.zsh.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "gentoo";
+      plugins = [ "git" "cp" ];
+    };
+    shellAliases = {
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit -m";
+    };
+  };
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
-
 
   # Git config using Home Manager modules
   programs.git = {
     enable = true;
     userName = "Stephen Harris";
     userEmail = "steveofar@hotmail.com";
-    aliases = {
-      c = "commit";
-      co = "checkout";
-      s = "status";
-    };
     extraConfig = {
       pull.ff = "only";
       init.defaultBranch = "main";
