f9c57880a40d45115af08c0f28cf248fc84b9564
Stephen Harris <steveofar@hotmail.com>
2023-02-26 20:48:19 -0600

    Add flake.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Home Manager Configuration
+
+## Apply updates made to `home.nix` or `flake.nix`:
+
+```sh
+cd $HOME/.config/nixpkgs && home-manager switch --flake .#macs
+```
+
+
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Home Manager configuration of scharris";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.macs = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
